c8bb8c618caaec64f1afca8e600a9f031065c5c4
rbatorowski <rbatorowski@pgs-soft.com>
2019-08-26 10:47:03 +0200

    Update cards to pass AllCardFromCardListPresentWithProperDescription

--- src/test/java/edu/bator/AllCardFromCardListPresentWithProperDescription.java
+++ src/test/java/edu/bator/AllCardFromCardListPresentWithProperDescription.java
@@ -7,7 +7,9 @@ import org.junit.Test;
 import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class AllCardFromCardListPresentWithProperDescription {
@@ -17,8 +19,11 @@ public class AllCardFromCardListPresentWithProperDescription {
         List<Card> actual = new AllCardsSet().getAllCards();
 
         expected.forEach(expectedCard -> {
-            assertTrue(expectedCard.getName() + " not found", actual.stream().anyMatch(actualCard -> Objects.equals(actualCard.getName(), expectedCard.getName())));
-            assertTrue(expectedCard.getName() + " description error", actual.stream().anyMatch(actualCard -> Objects.equals(actualCard.getDescription(), expectedCard.getDescription())));
+            Optional<Card> found = actual.stream().filter(actualCard -> Objects.equals(actualCard.getName(), expectedCard.getName())).findFirst();
+            assertTrue(expectedCard.getName() + " not found", found.isPresent());
+            String expectedDescription = expectedCard.getDescription() != null ? expectedCard.getDescription().replace("&nbsp;", " ") : expectedCard.getDescription();
+            String actualDescription = found.get().getDescription() != null ? found.get().getDescription().replace("&nbsp;", " ") : found.get().getDescription();
+            assertEquals(expectedCard.getName() + " description error", expectedDescription, actualDescription);
         });
     }
 }
