99733e65580d9d313a31ad731ba8dd739c29ffe6
rbatorowski <rbatorowski@pgs-soft.com>
2019-09-02 09:04:54 +0200

    Urgent Business - done

--- src/main/java/edu/bator/cards/done/UrgentBusiness.java
+++ src/main/java/edu/bator/cards/done/UrgentBusiness.java
@@ -6,6 +6,8 @@ import edu.bator.cards.Support;
 import edu.bator.game.GamePhase;
 import edu.bator.game.GameState;
 
+import java.util.Objects;
+
 public class UrgentBusiness extends Support implements Expirable {
 
     private int turnExpires;
@@ -20,7 +22,22 @@ public class UrgentBusiness extends Support implements Expirable {
 
     @Override
     public boolean preventsAllyOrHeroFromReadyingAttack(Card card, GameState gameState) {
-        return card.cardIsAHero();
+        return card.cardIsAHero() && gameState.allCardsInPlay().stream().anyMatch(c -> Objects.equals(this, c));
+    }
+
+    @Override
+    public void wasCasted(GameState gameState) {
+        super.wasCasted(gameState);
+        gameState.getYourHero().setAttackReadied(false);
+        gameState.getEnemyHero().setAttackReadied(false);
+        if (gameState.getGamePhase().equals(GamePhase.YOU_ACTION)) {
+            phaseExpires = GamePhase.ENEMY_END;
+            turnExpires = gameState.getCurrentTurn();
+        }
+        if (gameState.getGamePhase().equals(GamePhase.ENEMY_ACTION)) {
+            phaseExpires = GamePhase.YOU_END;
+            turnExpires = gameState.getCurrentTurn() + 1;
+        }
     }
 
     @Override
